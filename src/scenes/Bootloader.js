ae06bbf067341433a1ccf3e1c013d35d2db4c391
Ariel Curtido <aicurtido@gmail.com>
2020-04-17 13:54:27 -0400

    First commit

---
+++ src/scenes/Bootloader.js
@@ -0,0 +1,35 @@
+class Bootloader extends Phaser.Scene {
+    constructor() {
+        super('Bootloader');
+    }
+    init() {
+        console.log('Scene Bootloader');
+    }
+    preload() {
+        this.load.setPath('./assets/');
+        this.load.image([
+            'cero_opaco',
+            'cero',
+            'equis',
+            'equis_opaco',
+            'position',
+            'reload',
+            'tablero_win',
+            'tablero'
+        ]);
+        this.load.image('font', 'font/font.png');
+        this.load.json('fontConfig', 'font/font.json');
+
+        this.load.audio('draw', 'draw.mp3');
+        this.load.audio('pop', 'pop.mp3');
+        this.load.audio('win', 'win.mp3');
+
+        this.load.on('complete', () => {
+            const fontConfig = this.cache.json.get('fontConfig');
+            this.cache.bitmapFont.add('pixelFont', Phaser.GameObjects.RetroFont.Parse(this, fontConfig));
+            this.scene.start('Play');
+        });
+    }
+}
+
+export default Bootloader;
